fef2c76c1039826e974953701b2bbf91f8626866
Andrea Fiorucci <andrea.fiorucci@outlook.it>
2024-04-20 15:45:33 +0200

    initial GameController structure

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Models\Game;
+use App\Models\User;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $users = User::orderBy('name', 'asc')->get();
+        return view('games.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'position' => 'required',
+            'first_blood' => 'required',
+            'commander_kill' => 'required',
+            'multi_kill' => 'required',
+            'rescue' => 'required',
+            'estetics' => 'required',
+            'best_play' => 'required',
+        ]);
+
+        $request->user()->games()->create($validated);
+
+        return redirect(route('game.index'));
+ 
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Game $game)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Game $game)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Game $game)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Game $game)
+    {
+        //
+    }
+}
---
+++ resources/views/games/index.blade.php
@@ -0,0 +1,100 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <form method="POST" action="{{ route('games.store') }}">
+            @csrf
+
+            <div class="form-group">
+                <label for="position">Position:</label>
+                @foreach($users as $user)
+                <div style="text-align:right">
+                    <label>{{$loop->index}}:</label>
+                    <select id="position_{{$loop->index}}" name="position_{{$loop->index}}" class="form-control" required>
+                        <option value="">Select one</option>
+                        @foreach($users as $user)
+                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                @endforeach
+                <x-input-error :messages="$errors->get('position')" class="mt-2" />
+            </div>
+
+            <br>
+
+            <div class="form-group flex mb-2">
+                <label for="first_blood" class="mr-2">First Blood:</label>
+                <select id="first_blood" name="first_blood" class="form-control ml-auto" required>
+                    <option value="">Select one</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('first_blood')" class="mt-2" />
+            </div>
+
+            <div class="form-group flex mb-2">
+                <label for="commander_kill" class="mr-2">Commander Kill:</label>
+                <select id="commander_kill" name="commander_kill" class="form-control ml-auto" required>
+                    <option value="">Select one</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('commander_kill')" class="mt-2" />
+            </div>
+
+            <div class="form-group flex mb-2">
+                <label for="multi_kill" class="mr-2">Multi Kill:</label>
+                <select id="multi_kill" name="multi_kill" class="form-control ml-auto" required>
+                    <option value="">Select one</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('multi_kill')" class="mt-2" />
+            </div>
+
+            <div class="form-group flex mb-2">
+                <label for="rescue" class="mr-2">Rescue:</label>
+                <select id="rescue" name="rescue" class="form-control ml-auto" required>
+                    <option value="">Select one</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('rescue')" class="mt-2" />
+            </div>
+
+            <div class="form-group flex mb-2">
+                <label for="estetics" class="mr-2">Estetics/Theme:</label>
+                <select id="estetics" name="estetics" class="form-control ml-auto" required>
+                    <option value="">Select one</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('estetics')" class="mt-2" />
+            </div>
+
+            <div class="form-group flex mb-2">
+                <label for="best_play" class="mr-2">Best Play:</label>
+                <select id="best_play" name="best_play" class="form-control ml-auto" required>
+                    <option value="">Select one</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('best_play')" class="mt-2" />
+            </div>
+
+            <br>
+
+            <!-- <textarea name="message" placeholder="{{ __('What\'s on your mind?') }}" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">{{ old('message') }}</textarea> -->
+            <!-- <x-input-error :messages="$errors->get('message')" class="mt-2" /> -->
+
+            <div class="text-center">
+                <x-primary-button class="mt-4">{{ __('Submit') }}</x-primary-button>
+            </div>
+        </form>
+    </div>
+</x-app-layout>
